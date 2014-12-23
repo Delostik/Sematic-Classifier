26dbfaa8c076d2cc25a1572ff5d70bf5de4b10a5
delostik <delostik@126.com>
2014-12-23 14:48:48 +0800

    user index construction. add jqplot module

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -1,3 +1,28 @@
 <div class="container minh">
-
+    <div class="row">
+        <div class="col-md-3">
+            <div class="panel panel-default">
+                <!-- Default panel contents -->
+                <div class="panel-heading">Percentage</div>
+                <!-- Table -->
+                <table class="table table-hover">
+                    <tr>
+                        <td style="text-align:center;"> 
+                            <img height=200px width=220px />
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+        <div class="col-md-9">
+            <div class="panel panel-default">
+                <!-- Default panel contents -->
+                <div class="panel-heading">Latest news</div>
+                <!-- Table -->
+                <table class="table table-hover">
+                </table>
+            </div>
+        </div>
+    
+    </div>
 </div>
